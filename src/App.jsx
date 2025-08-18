76560deb1d432769e259af389333d63e96d3d350
YASINARAFAT88 <yasin80545859@gmail.com>
2025-08-18 23:15:40 +0600

    Adding blogs component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Header from './Header/Header'
+
+function App() {
+
+  return (
+    <>
+     <Header></Header>
+     <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
---
+++ src/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-6xl bg-red-300'>Vite + React</h1>
+        </div>
+    );
+};
+
+export default Header;
